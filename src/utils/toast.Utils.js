0121928cc64f9958723f1ee929f1a5efa55bae96
daidensacha <daidensacha@gmail.com>
2025-07-31 12:58:11 +0200

    Clean root directory: moved legacy files to backup_root_files/

--- src/utils/toast.Utils.js
+++
@@ -1,7 +0,0 @@
-import toast from "react-hot-toast";
-
-export const showLoadingToast = (msg) => toast.loading(msg, { id });
-export const showSuccessToast = (msg) => toast.loading(msg, { id });
-export const showErrorToast = (msg) => toast.loading(msg, { id });
-
-export const showToast = (msg) => toast(msg);
